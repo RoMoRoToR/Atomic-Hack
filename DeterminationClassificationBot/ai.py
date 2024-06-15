390cff63954a2a9f85c8f1bce4280a20982ed6c7
YaroslavIlin <e-mail_for_github@mail.ru>
2024-06-16 03:15:17 +0500

    DeterminationClassificationBot v.2

--- DeterminationClassificationBot/ai.py
+++ DeterminationClassificationBot/ai.py
@@ -1,2 +1,12 @@
-def predict(image_bytes: bytes) -> tuple[bytes, int]:
-    return b'', 1
+import cv2
+import numpy as np
+from ultralytics import YOLO
+
+model = YOLO("/content/BestFirst.pt")
+
+def predict(image_bytes: bytes, extension: str) -> bytes:
+   decoded = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), -1)
+   result = model(decoded)[0]
+   numpy_array = result.plot()
+   success, encoded_image = cv2.imencode(extension, numpy_array)
+   return encoded_image.tobytes()
